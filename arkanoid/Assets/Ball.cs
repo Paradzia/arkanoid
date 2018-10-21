fc32a89aa9fdbd6af7d1c045683c873ab23135a5
Parada <paradowskim94@gmail.com>
2018-10-21 22:13:58 +0200

    Bouncing scene

---
+++ arkanoid/Assets/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+	public float ballInitialVelocity = 600f;
+
+	private Rigidbody2D rb;
+	private bool ballInPlay;
+	
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
+			transform.parent = null;
+			ballInPlay = true;
+			rb.isKinematic = false;
+			rb.AddForce(new Vector2(ballInitialVelocity, ballInitialVelocity));
+		}
+	}
+}
