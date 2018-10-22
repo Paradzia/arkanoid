fd906d29d50ca9b67a785820644a99642585a5fb
Parada <paradowskim94@gmail.com>
2018-10-22 19:19:27 +0200

    destroyable bricks

---
+++ arkanoid/Assets/Scripts/Bricks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("predupa");
+        GameManager.instance.destroyBrick();
+        Destroy(gameObject);
+        Debug.Log("dupa");
+    }
+}
---
+++ arkanoid/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public int bricks = 20;
+	public GameObject bricksPrefab;
+	public GameObject paddle;
+	private GameObject clonePaddle;
+	public static GameManager instance = null;
+
+	
+	void Start () {
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		setup();
+	}
+
+	public void setup()
+	{
+		clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+		Instantiate(bricksPrefab, transform.position, Quaternion.identity);
+	}
+
+	public void destroyBrick()
+	{
+		bricks--;
+	}
+	
+}
