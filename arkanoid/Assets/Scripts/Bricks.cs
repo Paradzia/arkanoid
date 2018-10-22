481ea82ef6a515139b1935a6c2206a6be548038c
Parada <paradowskim94@gmail.com>
2018-10-22 19:21:34 +0200

    post-debug cleanup

--- arkanoid/Assets/Scripts/Bricks.cs
+++ arkanoid/Assets/Scripts/Bricks.cs
@@ -6,9 +6,7 @@ public class Bricks : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("predupa");
         GameManager.instance.destroyBrick();
         Destroy(gameObject);
-        Debug.Log("dupa");
     }
 }
