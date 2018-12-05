557af2aa3d94f6cbf6845a4e3e6381653c6df3b3
tirey93 <tirey93@gmail.com>
2018-12-05 18:05:03 +0100

    fixed paddle start position

--- arkanoid/Assets/Scripts/Paddle.cs
+++ arkanoid/Assets/Scripts/Paddle.cs
@@ -8,13 +8,20 @@ public class Paddle : MonoBehaviour
 	public float paddleSpeed = 1f;
 	
 	private Vector3 playerPos = new Vector3(0f, -0.2f, 0);
-	
-	void Update ()
-	{
-		float xTouchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x;
-		playerPos = new Vector3(Mathf.Clamp(xTouchPos, -1.47f, 1.43f), -4.5f, 0f);	
-		transform.position = playerPos;
-	}
 
-	
+    private void Start()
+    {
+        transform.position = new Vector3(Mathf.Clamp(0, -1.47f, 1.43f), -4.5f, 0f);
+    }
+
+    void Update ()
+	{
+        if(Input.touchCount > 0) {            
+            float xTouchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x;
+            playerPos = new Vector3(Mathf.Clamp(xTouchPos, -1.47f, 1.43f), -4.5f, 0f);	
+		    transform.position = playerPos;
+        }   
+    }
+
+
 }
